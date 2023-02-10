4d7f8db3bb3bdfb61ac4466726a1c2339c4b5216
HKS <hks.online@gmail.com>
2023-02-10 13:27:49 -0500

    return HTML in doGet()

--- src/com/hks/first/FirstServlet.java
+++ src/com/hks/first/FirstServlet.java
@@ -1,7 +1,11 @@
 package com.hks.first;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FirstServlet
  */
-@WebServlet("/FirstServlet")
+//@WebServlet("/FirstServlet")
+@WebServlet(description = "First Servlet", urlPatterns = {"/FirstServlet", "/FirstServlet.do"}, initParams = {@WebInitParam(name = "id", value = "1"), @WebInitParam(name = "name", value="hks")})
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	public static final String HTML_START="<html><body>";
+	public static final String HTML_END="</body></html>";
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,7 +35,10 @@ public class FirstServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		Date date = new Date();
+		out.println(HTML_START + "<h2>Hi There!</h2></br><h3>Date="+date+"</h3>" +HTML_END);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
